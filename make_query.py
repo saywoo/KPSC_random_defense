c2bca47984f622360de0bf936b89937112e04500
saywoo <saywoo1021@gmail.com>
2023-04-17 20:30:19 +0900

    2023/04/17 update

--- make_query.py
+++ make_query.py
@@ -2,28 +2,58 @@
 import json
 import requests
 
-# 멤버 리스트가 저장된 파일을 불러옴
-member_file = open("data/KPSC_member_list.txt", 'r')
+# 고를 문제의 개수를 지정
+problem_num = 8
 
-# 파일에서 멤버 리스트를 불러오고 푼 문제 순으로 정렬함
-member_list = member_file.readlines()
-member_file.close()
+# 리스트 선언
+member_list = []
+target_member_list = []
+difficulty_list = []
 
-# 멤버 리스트에서 '\n' 제거
-for i, member in enumerate(member_list):
-    member_list[i] = member.strip('\n')
+# 멤버 리스트가 저장된 파일에서 멤버 리스트를 불러옴
+file = open("data/KPSC_member_list.txt", 'r')
+for i in file:
+    member_list.append(i)
+file.close()
 
-for problem_num in range(8):
+# 문제별 타겟 멤버의 시작이 저장된 파일을 불러옴
+file = open("data/ban_member.txt", 'r')
+for i in file:
+    target_member_list.append(int(i))
+file.close()
+
+# 문제별 난이도가 저장된 파일을 불러옴
+file = open("data/difficulty.txt", 'r')
+for i in file:
+    difficulty_list.append(i)
+file.close()
+
+# 리스트에서 '\n' 삭제
+for i in range(len(member_list)):
+    member_list[i] = member_list[i][:-1]
+for i in range(problem_num):
+    difficulty_list[i] = difficulty_list[i][:-1]
+
+for i in range(problem_num):
     # 문제를 랜덤으로 검색할 쿼리를 작성
-    # 기본조건: 100명 이상이 푼 한국어 지문이 있는 문제
-    problem_search_query = "s#100..&lang:ko"
+    # 조건 1: 100명 이상이 푼 한국어 지문이 있는 문제
+    # 조건 2: 난이도가 difficulty_list[i]인 문제
+    problem_search_query = "s%23100..%26lang%3Ako" + "%26*" + difficulty_list[i]
 
-    # 모든 멤버가 풀지 않은 문제
-    for i, member in enumerate(member_list):
-        problem_search_query += "&!s@" + member
+    # 조건 3: (target_member_list[i]명부터 20명)이 안 푼 문제
+    for j in range(target_member_list[i], min(target_member_list[i] + 20, len(member_list))):
+        problem_search_query += "%26%21s%40" + member_list[j]
 
-    api_url = f"https://solved.ac/api/v3/search/problem?query={problem_search_query}&sort=random&direction=desc"
+    # solved.ac api에 쿼리를 요청
+    api_url = f"https://solved.ac/api/v3/search/problem?query={problem_search_query}&sort=random"
+    query_result = requests.get(api_url)
 
-    print(problem_search_query)
+    # 쿼리의 결과가 정상적으로 출력됐으면 랜덤으로 문제 하나를 출력
+    if query_result.status_code == requests.codes.ok:
+        query_json = json.loads(query_result.content.decode('utf-8'))
 
+        random_problem_id = query_json.get("items")[0].get("problemId")
 
+        print("problem count: " + str(query_json.get("count")) + ", problem id: " + str(random_problem_id))
+    else:
+        print("요청 실패")
